6bd9df0dd9559939db047fec0a93b84535d9e830
msklnk <moskalenko-lena@inbox.ru>
2024-11-26 19:16:18 +0400

    Добавлены исключения для таблицы Drinks

--- src/project/core/exceptions.py
+++ src/project/core/exceptions.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-
+# Client
 class UserNotFound(BaseException):
     _ERROR_MESSAGE_TEMPLATE: Final[str] = "Клиент с id {id} не найден"
     message: str
@@ -15,4 +15,22 @@ class UserAlreadyExists(BaseException):
 
     def __init__(self, email: str) -> None:
         self.message = self._ERROR_MESSAGE_TEMPLATE.format(email=email)
+        super().__init__(self.message)
+
+
+# Drinks
+class DrinkNotFound(BaseException):
+    _ERROR_MESSAGE_TEMPLATE: Final[str] = "Напиток с id {id} не найден"
+    message: str
+
+    def __init__(self, _id: int) -> None:
+        self.message = self._ERROR_MESSAGE_TEMPLATE.format(id=_id)
+        super().__init__(self.message)
+
+
+class DrinkAlreadyExists(BaseException):
+    _ERROR_MESSAGE_TEMPLATE: Final[str] = "Напиток с названием '{name}' уже существует"
+
+    def __init__(self, name: str) -> None:
+        self.message = self._ERROR_MESSAGE_TEMPLATE.format(name=name)
         super().__init__(self.message)
