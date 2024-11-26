e831f1dd4f7085d3a9bc07de3f4d14f42997a929
msklnk <moskalenko-lena@inbox.ru>
2024-11-26 19:24:32 +0400

    Добавлен DrinkRepository

--- src/project/infrastructure/postgres/repository/user_repo.py
+++ src/project/infrastructure/postgres/repository/user_repo.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text, select, insert, update, delete
 from sqlalchemy.exc import IntegrityError
 
-from project.schemas.user import ClientBase, ClientCreate, Client
-from project.infrastructure.postgres.models import Client
+from project.schemas.user import ClientCreate, Client, Drink, DrinkCreate
+from project.infrastructure.postgres.models import Client, Drink
 
-from project.core.exceptions import UserNotFound, UserAlreadyExists
+from project.core.exceptions import UserNotFound, UserAlreadyExists, DrinkNotFound, DrinkAlreadyExists
 
 
 class ClientRepository:
@@ -46,3 +46,95 @@ class ClientRepository:
             db.commit()
             return True
         return False
+
+
+class DrinkRepository:
+    _collection: Type[Drink] = Drink
+
+    async def check_connection(
+        self,
+        session: AsyncSession,
+    ) -> bool:
+        query = "select 1;"
+
+        result = await session.scalar(text(query))
+
+        return True if result else False
+
+    async def get_all_drinks(
+        self,
+        session: AsyncSession,
+    ) -> list[Drink]:
+        query = select(self._collection)
+
+        drinks = await session.scalars(query)
+
+        return [Drink.from_orm(drink) for drink in drinks.all()]
+
+    async def get_drink_by_id(
+        self,
+        session: AsyncSession,
+        drink_id: int,
+    ) -> Drink:
+        query = (
+            select(self._collection)
+            .where(self._collection.drinkid == drink_id)
+        )
+
+        drink = await session.scalar(query)
+
+        if not drink:
+            raise DrinkNotFound(_id=drink_id)
+
+        return Drink.from_orm(drink)
+
+    async def create_drink(
+        self,
+        session: AsyncSession,
+        drink: DrinkCreate,
+    ) -> Drink:
+        query = (
+            insert(self._collection)
+            .values(drink.dict(exclude_unset=True))
+            .returning(self._collection)
+        )
+
+        try:
+            created_drink = await session.scalar(query)
+            await session.flush()
+        except IntegrityError:
+            raise DrinkAlreadyExists(name=drink.name)
+
+        return Drink.from_orm(created_drink)
+
+    async def update_drink(
+        self,
+        session: AsyncSession,
+        drink_id: int,
+        drink: DrinkCreate,
+    ) -> Drink:
+        query = (
+            update(self._collection)
+            .where(self._collection.drinkid == drink_id)
+            .values(drink.dict(exclude_unset=True))
+            .returning(self._collection)
+        )
+
+        updated_drink = await session.scalar(query)
+
+        if not updated_drink:
+            raise DrinkNotFound(_id=drink_id)
+
+        return Drink.from_orm(updated_drink)
+
+    async def delete_drink(
+        self,
+        session: AsyncSession,
+        drink_id: int
+    ) -> None:
+        query = delete(self._collection).where(self._collection.drinkid == drink_id)
+
+        result = await session.execute(query)
+
+        if not result.rowcount:
+            raise DrinkNotFound(_id=drink_id)
