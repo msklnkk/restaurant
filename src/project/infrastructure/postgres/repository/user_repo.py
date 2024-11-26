61d0d6d723950ff9ff616c4c85a11cecb0246298
msklnk <moskalenko-lena@inbox.ru>
2024-11-26 19:43:26 +0400

    Добавлен репозиторий для Price

--- src/project/infrastructure/postgres/repository/user_repo.py
+++ src/project/infrastructure/postgres/repository/user_repo.py
@@ -5,11 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text, select, insert, update, delete
 from sqlalchemy.exc import IntegrityError
 
-from project.schemas.user import ClientCreate, Client, Drink, DrinkCreate
-from project.infrastructure.postgres.models import Client, Drink
+from project.schemas.user import ClientCreate, Client, Drink, DrinkCreate, Price, PriceCreate
+from project.infrastructure.postgres.models import Client, Drink, Price
 
 from project.core.exceptions import UserNotFound, UserAlreadyExists, DrinkNotFound, DrinkAlreadyExists
-
+from project.core.exceptions import PriceNotFound, PriceAlreadyExists
 
 class ClientRepository:
     @staticmethod
@@ -138,3 +138,97 @@ class DrinkRepository:
 
         if not result.rowcount:
             raise DrinkNotFound(_id=drink_id)
+
+
+# Price
+
+class PriceRepository:
+    _collection: Type[Price] = Price
+
+    async def check_connection(
+        self,
+        session: AsyncSession,
+    ) -> bool:
+        query = "select 1;"
+
+        result = await session.scalar(text(query))
+
+        return True if result else False
+
+    async def get_all_prices(
+        self,
+        session: AsyncSession,
+    ) -> list[Price]:
+        query = select(self._collection)
+
+        prices = await session.scalars(query)
+
+        return [Price.from_orm(price) for price in prices.all()]
+
+    async def get_price_by_id(
+        self,
+        session: AsyncSession,
+        dishid: int,
+    ) -> Price:
+        query = (
+            select(self._collection)
+            .where(self._collection.dishid == dishid)
+        )
+
+        price = await session.scalar(query)
+
+        if not price:
+            raise PriceNotFound(_id=dishid)
+
+        return Price.from_orm(price)
+
+    async def create_price(
+        self,
+        session: AsyncSession,
+        price: PriceCreate,
+    ) -> Price:
+        query = (
+            insert(self._collection)
+            .values(price.dict(exclude_unset=True))
+            .returning(self._collection)
+        )
+
+        try:
+            created_price = await session.scalar(query)
+            await session.flush()
+        except IntegrityError:
+            raise PriceAlreadyExists(dish_id=created_price.dishid)
+
+        return Price.from_orm(created_price)
+
+    async def update_price(
+        self,
+        session: AsyncSession,
+        dishid: int,
+        price: PriceCreate,
+    ) -> Price:
+        query = (
+            update(self._collection)
+            .where(self._collection.dishid == dishid)
+            .values(price.dict(exclude_unset=True))
+            .returning(self._collection)
+        )
+
+        updated_price = await session.scalar(query)
+
+        if not updated_price:
+            raise PriceNotFound(_id=dishid)
+
+        return Price.from_orm(updated_price)
+
+    async def delete_price(
+        self,
+        session: AsyncSession,
+        dishid: int
+    ) -> None:
+        query = delete(self._collection).where(self._collection.dishid == dishid)
+
+        result = await session.execute(query)
+
+        if not result.rowcount:
+            raise PriceNotFound(_id=dishid)
